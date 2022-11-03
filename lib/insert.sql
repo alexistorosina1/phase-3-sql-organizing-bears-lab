ee55af9448b1d01c64cee5debd85d8a67b4d3723
Alexis Torosina <alexistorosina1@gmail.com>
2022-11-03 19:17:25 -0400

    commit tree 3d0f94ecfd81326bf0668e418fdd1bbbd30bff40

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES(1, "Mr.chocolate", 2, "M", "brown", "calm", 1);
+INSERT INTO bears VALUES(2, "Rowdy", 2, "M", "spots", "calm", 1);
+INSERT INTO bears VALUES(3, "Mr.chocolate", 2, "M", "brown", "calm", 1);
+INSERT INTO bears VALUES(4, "Sergeant Brown", 20, "F", "brown", "hyper", 1);
+INSERT INTO bears VALUES(5, "Melissa", 20, "F", "white", "hyper", 0);
+INSERT INTO bears VALUES(6, "Grinch", 14, "F", "brown", "calm", 1);
+INSERT INTO bears VALUES(7, "Wendy", 9, "F", "brown", "hyper", 1);
+INSERT INTO bears VALUES(8,null, 20, "M", "black", "aggressive", 0);
